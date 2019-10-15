0c44d82ca276d50b9bafea9def79e27ee2fe87c8
Irvin Castellanos <irvin.py@gmail.com>
2019-10-14 23:23:18 -0500

    Add metrobus service

--- getdata.rb
+++ getdata.rb
@@ -1,27 +1,22 @@
-require 'protobuf'
-require 'google/transit/gtfs-realtime.pb'
-require 'net/http'
-require 'uri'
-
 require './src/models/schedule'
 require './src/storage/firestore'
 require './src/storage/mongo'
+require './src/service/metrobus_gtfs'
 
-data = Net::HTTP.get(URI.parse("http://app.citi-mb.mx/GTFS-RT/vehiculosPosicion"))
-feed = Transit_realtime::FeedMessage.decode(data)
+ms = MetrobusGTFS.new
+feed = ms.feed
 
 #  for entity in feed.entity do
 #    p entity
 #  end
 
-# p feed.entity[0].vehicle.position
 p feed.entity.count
 
-schedule = Schedule.new(feed.entity[0])
+# schedule = Schedule.new(feed.entity[0])
 
 # f = Firestore.new
 # f.save(schedule)
 
-m = Mongodb.new
-m.save schedule
+# m = Mongodb.new
+# m.save schedule
 
